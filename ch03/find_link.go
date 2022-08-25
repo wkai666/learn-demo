b7663e57e134d6b0097d184da5e63b7ad442ccc2
wangkai <wangkai_2015@126.com>
2022-08-25 12:22:29 +0800

    add ch03

---
+++ ch03/find_link.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"golang.org/x/net/html"
+)
+
+func main() {
+	doc, err := html.Parse(os.Stdin)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "findlink1: %v\n", err)
+		os.Exit(1)
+	}
+
+	for _, link := range visit(nil, doc) {
+		fmt.Println(link)
+	}
+}
+
+// visit appends to links each link found in n and returns the result.
+func visit(links []string, n *html.Node) []string {
+	if n.Type == html.ElementNode && n.Data == "a" {
+		for _, a := range n.Attr {
+			if a.Key == "href" {
+				links = append(links, a.Val)
+			}
+		}
+	}
+
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		links = visit(links, c)
+	}
+
+	return links
+}
+
+func outline(stack []string, n *html.Node) {
+	if n.Type == html.ElementNode {
+		stack = append(stack, n.Data) // push tag
+		fmt.Println(stack)
+	}
+
+	for c := n.FirstChild; c != nil; c = n.NextSibling {
+		outline(stack, c)
+	}
+}
---
+++ ch03/go.mod
@@ -0,0 +1,5 @@
+module ch03
+
+go 1.16
+
+require golang.org/x/net v0.0.0-20220822230855-b0a4917ee28c
