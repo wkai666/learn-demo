91e6d51cf2d656dd75c699c0db53382723651872
wangkai <wangkai_2015@126.com>
2022-08-30 10:30:12 +0800

    add http switch

--- ch05/http.go
+++ ch05/http.go
@@ -14,13 +14,34 @@ func (d dollars) String() string {
 
 type database map[string]dollars
 
+// func (db database) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+// 	for item, price := range db {
+// 		fmt.Fprintf(w, "%s:%s\n", item, price)
+// 	}
+// }
+
 func (db database) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	for item, price := range db {
-		fmt.Fprintf(w, "%s:%s\n", item, price)
+	switch req.URL.Path {
+	case "/list":
+		for item, price := range db {
+			fmt.Fprintf(w, "%s: %s\n", item, price)
+		}
+	case "/price":
+		item := req.URL.Query().Get("item")
+		price, ok := db[item]
+		if !ok {
+			w.WriteHeader(http.StatusNotFound)
+			fmt.Fprintf(w, "no such item: %q\n", item)
+			return
+		}
+		fmt.Fprintf(w, "%s\n", price)
+	default:
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "no such page: %s\n", req.URL)
 	}
 }
 
 func main() {
-	db := database{"shoe": 50, "socks": 30}
+	db := database{"shoes": 50, "socks": 30}
 	log.Fatal(http.ListenAndServe("localhost:8080", db))
 }
