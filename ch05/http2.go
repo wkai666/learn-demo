f2d8555f12fe10887bb2b9c7bffc4a0d323c17bd
wangkai <wangkai_2015@126.com>
2022-08-30 10:52:22 +0800

    update http

---
+++ ch05/http2.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type dollars float32
+
+func (d dollars) String() string {
+	return fmt.Sprintf("$%.2f", d)
+}
+
+type database map[string]dollars
+
+// func (db database) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+// 	for item, price := range db {
+// 		fmt.Fprintf(w, "%s:%s\n", item, price)
+// 	}
+// }
+
+// func (db database) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+// 	switch req.URL.Path {
+// 	case "/list":
+// 		for item, price := range db {
+// 			fmt.Fprintf(w, "%s: %s\n", item, price)
+// 		}
+// 	case "/price":
+// 		item := req.URL.Query().Get("item")
+// 		price, ok := db[item]
+// 		if !ok {
+// 			w.WriteHeader(http.StatusNotFound)
+// 			fmt.Fprintf(w, "no such item: %q\n", item)
+// 			return
+// 		}
+// 		fmt.Fprintf(w, "%s\n", price)
+// 	default:
+// 		w.WriteHeader(http.StatusNotFound)
+// 		fmt.Fprintf(w, "no such page: %s\n", req.URL)
+// 	}
+// }
+
+func (db database) list(w http.ResponseWriter, req *http.Request) {
+	for item, price := range db {
+		fmt.Fprintf(w, "%s: %s\n", item, price)
+	}
+}
+
+func (db database) price(w http.ResponseWriter, req *http.Request) {
+	item := req.URL.Query().Get("item")
+	price, ok := db[item]
+	if !ok {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "no such item: %q\n", item)
+		return
+	}
+
+	fmt.Fprintf(w, "%s\n", price)
+}
+
+func main() {
+	db := database{"shoes": 50, "socks": 30}
+	mux := http.NewServeMux()
+	mux.Handle("/list", http.HandlerFunc(db.list))
+	mux.Handle("/price", http.HandlerFunc(db.price))
+	log.Fatal(http.ListenAndServe("localhost:8080", mux))
+}
